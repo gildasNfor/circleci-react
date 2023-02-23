42909376e856e66d30a69562861df51503a85e3f
gildasNfor <gildasnfor@gmail.com>
2023-02-23 10:11:30 +0000

    Updates

---
+++ precache-manifest.0aaeb45928481f9248adbf1ac62d7e9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa8f78928042082df3fc",
+    "url": "/static/css/main.18393e78.chunk.css"
+  },
+  {
+    "revision": "fa8f78928042082df3fc",
+    "url": "/static/js/main.7f4bb8ce.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "987698383b2e432aa97c2edde3d12037",
+    "url": "/index.html"
+  }
+];
